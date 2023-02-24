2050ff95a7ed4c52794156e2ce9f0263251eda8e
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-02-24 16:50:23 +0100

    router

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 
-import headerStyle from './Header.module.css'
+import headerStyle from './Header.module.css';
+import '../../App.css'
 
 
 const Header = ()=>{
-    return (<header>
-        <section >
-            <p className={headerStyle.subscribe}><a href="#subscribe">Subscribe to our weather newsletter</a></p>
-            <div className={headerStyle.banner}>
-                <img className={headerStyle.img} src="https://static01.nyt.com/images/2014/12/11/technology/personaltech/11machin-illo/11machin-illo-superJumbo-v3.jpg" alt="weather"/>
-                <h1 className={headerStyle.title}>Weather Website</h1>
-            </div>
-        </section>
-       
+
+    return (
+    <header>
+        <NavLink  to='/subscribe' className={headerStyle.subscribe}>Subscribe to receive our newsletter</NavLink>
+        <NavLink  to='/' className={headerStyle.subscribe}>HOME</NavLink>
+        <div className={headerStyle.banner}>
+            <img className={headerStyle.img} src="https://static01.nyt.com/images/2014/12/11/technology/personaltech/11machin-illo/11machin-illo-superJumbo-v3.jpg" alt="weather"/>
+            <h1 className={headerStyle.title}>Weather Website</h1>
+        </div>
     </header>)
-    
 }
 
 export default Header;
